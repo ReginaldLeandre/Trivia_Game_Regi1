f04666ed1e4b3fc7920845328c81ea2c7701bf97
rleandre <leandrereginald@gmail.com>
2023-07-26 09:44:16 -0400

    check line 157 for comment

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ let score = 0;
 //
 let currentQuestion = 0;
 
+const listItem = document.createElement("li");
+const choicesInput = document.createElement("input");
+
 
 //this questions array will hold the question, answers, and choices
 //
@@ -57,6 +60,7 @@ const AOTquestions = [
 ];
 
 
+
 //this will create variables to hold the values of each array value
 //at the index of current question
 //
@@ -67,7 +71,7 @@ const answersChange = AOTquestions[currentQuestion].answers;
 //this will contain the div with the id question-container to display the question
 // 
 const questionContainer = document.getElementById("question-container");
-
+const enterButton = document.getElementById("Enter-btn")
 
 //this variable will create an H3 element to place the question in
 //the second line will create an unordered list element for the questions
@@ -77,7 +81,8 @@ const choicesList = document.createElement("ul");
 
 
 
-
+//array to store the selected answers
+const selectedAnswers = [];
 
 
 /*--------------Other Tools and built in functions--------------*/
@@ -85,33 +90,33 @@ const choicesList = document.createElement("ul");
 
 
 /* --------------functions--------------*/
-
-// this function will display the question and choices
-//first we select the div containing the question with the id question-container
-//then we use innerHTML to to map the the values the question and choices indexes
-// function displayQuestion() {
+/*
+this function will display the question and choices
+first we select the div containing the question with the id question-container
+then we use innerHTML to to map the the values the question and choices indexes
+function displayQuestion() {
     
-//     const  questionsChange = AOTquestions[currentQuestion].question;
-//     const choicesChange = AOTquestions[currentQuestion].choices
+    const  questionsChange = AOTquestions[currentQuestion].question;
+    const choicesChange = AOTquestions[currentQuestion].choices
 
-//     //questionContainer.innerHTML( questionsChange)
-//     //choicesChanges.map((choice, index) =>
+    //questionContainer.innerHTML( questionsChange)
+    //choicesChanges.map((choice, index) =>
 
-//     //console.log( questionsChange)
-// };
+    //console.log( questionsChange)
+};
 
-// displayQuestion();
+displayQuestion();
+*/
+function displayQuestion() {
+       //this variable is to dynamically insert the questions in an h2 element we created above
+    questionHeading.textContent = AOTquestions[currentQuestion].question;
 
-
-//this variable is to dynamically insert the questions in an h2 element we created above
-questionHeading.textContent = AOTquestions[currentQuestion].question;
-
-//here we append the text content created in above to the questionContainer 
-//so that it is inside the div
-//
-questionContainer.appendChild(questionHeading)
-
-
+    //here we append the text content created in above to the questionContainer 
+    //so that it is inside the div
+    //
+    questionContainer.appendChild(questionHeading)
+}    
+ 
 //this will iterate through my AOTquestions array's choices within each array object 
 //and create a list item for each one
 //each list item will have a type of radio, etc.
@@ -120,25 +125,96 @@ questionContainer.appendChild(questionHeading)
 //interate through the indexes
 //then finally append all items 
 //
-choicesChange.forEach(function(choice, index) { 
-    const listItem = document.createElement("li")
-    const choicesInput = document.createElement("input")
-    choicesInput.type = "radio";
-    choicesInput.name = "choice";
-    choicesInput.value = choice
-    choicesInput.id = 'choice${index +1}' 
+
+function displayChoices() {
+
+    choicesChange.forEach(function(choice, index) { 
+        let currentChoice = null;
+        const listItem = document.createElement("li");
+        const choicesInput = document.createElement("input");
+
+        choicesInput.type = "radio";
+        choicesInput.name = "choice";
+        choicesInput.value = choice
+        choicesInput.id = 'choice${index +1}' 
     
     
-    const choiceLabel = document.createElement("label");
-    choiceLabel.textContent = choice;
-    choiceLabel.setAttribute("for", 'choice${index +1}');
+        const choiceLabel = document.createElement("label");
+        choiceLabel.textContent = choice;
+        choiceLabel.setAttribute("for", 'choice${index +1}');
 
 
-    listItem.appendChild(choicesInput);
-    listItem.appendChild(choiceLabel);
-    choicesList.appendChild(listItem);
+        listItem.appendChild(choicesInput);
+        listItem.appendChild(choiceLabel);
+        choicesList.appendChild(listItem);
+        
+       const chosenAnswer = choicesInput.addEventListener("change", (e) => {
+                currentChoice = e.target.value;
+        });
+        console.log
+        enterButton.addEventListener("click", (chosenAnswer) =>{
+            if(choicesInput.addEventListener("change", (e) => {
+                currentChoice = e.target.value}) === AOTquestions[currentQuestion].answers){
+
+            score++
+            console.log(score)
+        }
+        //this is to incremement the current question so that 
+        //it will show the next question after
+        
+        
+    
+        // if(currentQuestion < AOTquestions.length){
+        //     displayQuestion()
+        // }
+        // else {
+        //     endGame()
+        // }
+    
+    
+})
+        
+            
+            
+
+
+        // console.log(choicesInput)
+
+
 });
 
 
 //this is to append the results of the foreach loop for the choices into the question container 
 questionContainer.appendChild(choicesList)
+
+
+
+
+}
+
+
+//calling the display question function may need to put all functions in an emcompassing function 
+//calling init
+displayChoices()
+
+/*
+this function will check the selected answer's index value compare 
+it to the index value of the correct answer
+then give a score based on if those values match
+
+this function will also end the game if the value of current question exceeds
+the lenght of indexes in the array AOTquestions
+
+i will need to add an event listener to the button tag in the HTML document 
+so that it will call this method
+ */
+
+
+
+//calling the check answer function
+//checkAnswer() 
+
+
+function endGame() {
+
+}
