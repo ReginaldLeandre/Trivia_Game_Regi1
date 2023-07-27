f16c45db71bbc5e7331ca08ab93f2302708c86b9
rleandre <leandrereginald@gmail.com>
2023-07-27 01:31:17 -0400

    accident push earlier, MVP status now

--- js/main.js
+++ js/main.js
@@ -1,15 +1,12 @@
-/*-------------- Variable --------------*/
-// will hold player's score
-//
-let score = 0;
+/* -------- Variables and Arrays--------- */
 
-//will hold the value of the currenct question
-//
-let currentQuestion = 0;
+//this variable will store the data of the selected choice of the user in the displayQuestions function so that it can be evaluated 
+//by the checkAnswer function,
+// let currentChoice = null;
 
-const listItem = document.createElement("li");
-const choicesInput = document.createElement("input");
 
+// const listItem = document.createElement("li");
+// const choicesInput = document.createElement("input");
 
 //this questions array will hold the question, answers, and choices
 //
@@ -19,183 +16,194 @@ const AOTquestions = [
         choices: ["Petra Ral", "Rico Brzenska", "Annie Leonhart", "Hange Zoe"],
         answers: "Petra Ral"
     },
-    /* 
-    {
-        question: "In Attack on Titan Season 4 Episode 19, Two Brothers, who were the 3 sets of brothers. (select all that apply)",
-        choices: ["Eren and Zeke", "Reiner and Armin", "Falco and Colt", "Porco and Marcel"],
-        answers: [0, 2, 3]
-    },
+    
     {
         question: "At the end of season 2, Why was Eren able to use the founder's power?",
         choices: ["He touched Dina Fritz", "Hannes's death motivated him", "He always could use it", "Erwin's speach motivated him"],
-        answers: 
+        answers: "He touched Dina Fritz"
     },
     {
-        question: "When motivates Grisha Jaegar to join the Eldian Restorationists",
+        question: "What motivates Grisha Jaegar to join the Eldian Restorationists?",
         choices: ["When he found out about sister", "When he found out Zeke was born", "When Eren was born", "When he arrived outside the Wall Maria"],
-        answers: 0
-    },
+        answers: "When he found out about sister"
+    }, 
     {
         question: "Why did the titan kneel to Ilse Langnar",
         choices: ["She looked like Ymir", "It was dark, titans need sun to move", "It was healing it's wounds", "No reason, titans just do things"],
-        answers: 0
+        answers: "She looked like Ymir"
     },
     {
         question: "What was the name of Levi's mom?",
         choices: ["Kuchel Ackerman", "Mikasa Ackermen", "Kenny Ackerman", "Historia Reiss"],
-        answers: 0
+        answers: "Kuchel Ackerman"
     },
     {
         question: "In season 4 episode 20, why did Eren get so angry when Freida Reiss was speaking?",
         choices: ["she was cursing him out", "Freida stated that eldians do not deserve to live", 
                 "frieda had the founding titan", "frieda was under the influence of the first king's will"],
-        answers: 1
+        answers: "Freida stated that eldians do not deserve to live"
     },
     {
         question: "What caused the Marleyan persecution and enslavement of Eldians occur?",
         choices: ["the first king gave up the fight", "Ymir Fritz's death", "Historia accepted her family name", "Kenny Ackerman murdered Reeve's comapny CEO"],
-        answers: 0
+        answers: "the first king gave up the fight"
     }
-    */
+    
 ];
 
 
-
-//this will create variables to hold the values of each array value
-//at the index of current question
+// will hold player's score
 //
-const  questionsChange = AOTquestions[currentQuestion].question;
-const choicesChange = AOTquestions[currentQuestion].choices;
-const answersChange = AOTquestions[currentQuestion].answers;
+let score = 0;
 
-//this will contain the div with the id question-container to display the question
-// 
-const questionContainer = document.getElementById("question-container");
-const enterButton = document.getElementById("Enter-btn")
-
-//this variable will create an H3 element to place the question in
-//the second line will create an unordered list element for the questions
+//will hold the value of the currenct question
 //
-const questionHeading = document.createElement("h3")
-const choicesList = document.createElement("ul");
+let currentQuestion = 0;
+
+// //this will hold the document elements so that the questions can be displayed in the appropriate location on the UI
+// const questionElement = document.getElementById("question-container");
+// const choicesElement = document.getElementById("choices-container");
+
+// //this will create variables to hold the values of each array value
+// //at the index of current question
+// //
+// const  questionsChange = AOTquestions[currentQuestion].question;
+// const choicesChange = AOTquestions[currentQuestion].choices;
+// const answersChange = AOTquestions[currentQuestion].answers;
+
+// //this will contain the div with the id question-container to display the question
+// // 
+// const questionContainer = document.getElementById("question-container");
+// const enterButton = document.getElementById("Enter-btn")
+
+// //this variable will create an H3 element to place the question in
+// //the second line will create an unordered list element for the questions
+// //
+// const questionHeading = document.createElement("h3")
+// const choicesList = document.createElement("ul");
 
 
 
-//array to store the selected answers
-const selectedAnswers = [];
+// //array to store the selected answers
+// const selectedAnswers = [];
 
 
 /*--------------Other Tools and built in functions--------------*/
 
 
 
+
+
+
+
 /* --------------functions--------------*/
-/*
-this function will display the question and choices
-first we select the div containing the question with the id question-container
-then we use innerHTML to to map the the values the question and choices indexes
-function displayQuestion() {
-    
-    const  questionsChange = AOTquestions[currentQuestion].question;
-    const choicesChange = AOTquestions[currentQuestion].choices
 
-    //questionContainer.innerHTML( questionsChange)
-    //choicesChanges.map((choice, index) =>
+//this function targets a document element and sets the CSS display property to "block" 
+//which will help with filling out the complete div width
+//conversly hidenextbuntton function will use the css property display set to "none" so it doesnt show
+function showNextButton() {
+    const nextBtn = document.getElementById("nextBtn");
+    nextBtn.style.display = "block";
+  }
 
-    //console.log( questionsChange)
-};
 
-displayQuestion();
-*/
-function displayQuestion() {
-       //this variable is to dynamically insert the questions in an h2 element we created above
-    questionHeading.textContent = AOTquestions[currentQuestion].question;
+function hideNextButton() {
+    const nextBtn = document.getElementById("nextBtn");
+    nextBtn.style.display = "none";
+  }
+  
 
-    //here we append the text content created in above to the questionContainer 
-    //so that it is inside the div
-    //
-    questionContainer.appendChild(questionHeading)
-}    
- 
-//this will iterate through my AOTquestions array's choices within each array object 
-//and create a list item for each one
-//each list item will have a type of radio, etc.
-//and then
-//text content at whatever index of that choice item at that index
-//interate through the indexes
-//then finally append all items 
+
+//this function will pull the data from the question property of each array object and display them with the .textContent function
 //
+function displayQuestion() {
 
-function displayChoices() {
+    //this variable will dynamically display each question using the value of currentQuestion as its index
+    const displayingQuestion = AOTquestions[currentQuestion];
+  
 
-    choicesChange.forEach(function(choice, index) { 
-        let currentChoice = null;
-        const listItem = document.createElement("li");
-        const choicesInput = document.createElement("input");
+//text content 
+    questionElement.textContent = displayingQuestion.question;
 
-        choicesInput.type = "radio";
-        choicesInput.name = "choice";
-        choicesInput.value = choice
-        choicesInput.id = 'choice${index +1}' 
+//Clearing the answer choices from the previous question
+    choicesElement.innerHTML = "";
+  
+
+//Creating buttons for each answer choice 
+//and adding the text content to match the value of the choice property
+//then add an event listener that calls the checkAnswer function created below 
+//then we append the value of choice button to the choiceElement created above so that the ui shows these selections
+    displayingQuestion.choices.forEach((choice) => {
+        const choiceButton = document.createElement("button");
+        choiceButton.textContent = choice;
+        choiceButton.addEventListener("click", checkAnswer);
+        choicesElement.appendChild(choiceButton);
+    });
+  }
+
+
+
+
+
+//this function will be used to evaluate the answer given by the UI to the value withing the array
+//the event argument allows us to access information about the event happening ie; clicks 
+//then the score will increment if the user selected the correct answer.
+function checkAnswer(event) {
+
+    //this assigns the text content to a variable so that we can use it to compare values
+    const selectedChoice = event.target.textContent;
+    //this is the value we are comparing to current question will dynamically increment
+    const displayingQuestion = AOTquestions[currentQuestion];
     
-    
-        const choiceLabel = document.createElement("label");
-        choiceLabel.textContent = choice;
-        choiceLabel.setAttribute("for", 'choice${index +1}');
 
+    //if the value of rhe selection matches the value of the current answer
+    if (selectedChoice === displayingQuestion.answers) {
+        score++;
+    }
+  
 
-        listItem.appendChild(choicesInput);
-        listItem.appendChild(choiceLabel);
-        choicesList.appendChild(listItem);
-        
-       const chosenAnswer = choicesInput.addEventListener("change", (e) => {
-                currentChoice = e.target.value;
-        });
-        console.log
-        enterButton.addEventListener("click", (chosenAnswer) =>{
-            if(choicesInput.addEventListener("change", (e) => {
-                currentChoice = e.target.value}) === AOTquestions[currentQuestion].answers){
-
-            score++
-            console.log(score)
-        }
-        //this is to incremement the current question so that 
-        //it will show the next question after
-        
-        
-    
-        // if(currentQuestion < AOTquestions.length){
-        //     displayQuestion()
-        // }
-        // else {
-        //     endGame()
-        // }
-    
-    
-})
-        
-            
-            
-
-
-        // console.log(choicesInput)
-
-
-});
-
-
-//this is to append the results of the foreach loop for the choices into the question container 
-questionContainer.appendChild(choicesList)
+    //here we call the hide function so that 
+    hideChoices();
+  }
 
 
 
+//this will select all buttons with the choice-container id and remove listeners after the answer has been eval-ed
+function hideChoices() {
 
-}
+    //we use the querySelectorAll built in to select all the button and assigns it to the choice button varibale 
+    const choiceButtons = document.querySelectorAll("#choices-container button");
+
+    //then we use the for each method to iterate through the container for each button element
+    //listener from the check answer function created above
+    //this will make it so  checkasnwer function will no longer be called on the event of clicking
+    choiceButtons.forEach((button) => {
+        button.removeEventListener("click", checkAnswer);
+    });
+
+    //then we call the show next button
+    showNextButton();
+  }  
 
 
 //calling the display question function may need to put all functions in an emcompassing function 
 //calling init
-displayChoices()
+
+
+//this function will assist iterating through the array with the currentQuestion variable as its index
+//also it will verify if the currentQuestion value is 
+//it will also call the displayQuestion function and hide Nextbutton function so that the user can make a descision 
+//without actidentally pressing the next button
+//if the current question variable exceeds the lenght of the array it will show the user the final score with showresult
+function nextQ() {
+    currentQuestion++;
+    if (currentQuestion < AOTquestions.length) {
+        displayQuestion();
+        hideNextButton();
+    } else {
+        showResults();
+    }
+  }
 
 /*
 this function will check the selected answer's index value compare 
@@ -212,9 +220,31 @@ so that it will call this method
 
 
 //calling the check answer function
-//checkAnswer() 
 
 
-function endGame() {
+
+//with query selector we will select the first document with the class of container
+//then we insert the text concatenated with the score the user got right(incremement value from checkAnswer) and the array lenght of AOT questions
+//should read "Results (new line) You score # out of #!"
+function showResults() {
+    const container = document.querySelector(".container");
+    container.innerHTML = `
+        <h1>Results:</h1>
+        <p>You scored ${score} out of ${AOTquestions.length}!</p>
+    `;
+  }  
+
+
+//initialize the game  
+function init() {
+    displayQuestion();
+    hideNextButton();
+      
+
 
 }
+
+init()
+
+
+
--- planning.md
+++ planning.md
@@ -14,3 +14,41 @@
 -display a message asking the user if they want to play again, if yes  
     restart the game if no display a 
     "thanks for playing message" and end the application.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    1. A user can see information about the game - title, instructions etc (HTML/CSS) and question data should be loaded in JS (init(). 
+- Build static HTML / Layout CSS
+- Have initialized variables and init() function setting initial values (currentQuestion = 0, etc)
+- In your init() provide your buttons with a click event listener - that runs a stubbed function (handleClick)
+
+2. A user can see one question with some way of selecting from multiple answers (radio + submit button) or a button click (DOM update - render () function)
+- create a renderQuestion() that populates the question heading content of your HTML with the currentQuestion content
+- create a renderOptions() that populates the current answer options (textContent of the button) for your current Question 
+- test that the button clicked on returns the text content and store it in a 'currentGuess' variable 
+
+3. When a user selects the answer the game should evaluate if the guess selected is correct and notify the player (a highlighted color, score increase, and/or status message) (evaluateAnswer + render())
+- inside of the handleClick function you should compare the selected answer value against the currentQueestion's answer property- if the answer is correct or incorrect update the scoring values
+- create a renderOutcome function that changes the UI to let the player know if they answered correctly, or not
+- invoke the renderOutcomes
+
+4. After the answer status is updated, a user should be shown a new question and answer combinations (changeQuestion() + render())
+- changeQuestion should do the following
+	- clear out current guess value 
+	- increment the index of the Current question
+	- if the currentQuestion is defined (the index value does not exceed the length of the questions list) call renderQuestion()
+	   - else call gameOver()
+	- update the player that is game over and tell them how many questions they got right / wrong
+
+5. Game over - hide the last question and show the score along with a 'restart button' when the button is clicked it should refresh the page, or re-run your init function and start the game over. 
